82dca7e6e249f74a7dc01d47d909e6bbb4a9c0d2
menezes-dev <menezes.icet@gmail.com>
2024-01-19 21:21:57 -0400

    feat: adição do arquivo de configurações do vercel

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -1,8 +1,14 @@
 import { hash } from "bcryptjs";
 import { CreateUser, UserDocument } from "../../interfaces/user.interfaces";
 import UserModel from "../../models/user.model";
+import AppError from "../../errors/appError";
 
 const createUserService = async (data: CreateUser): Promise<UserDocument> => {
+  const verifyEmail = await UserModel.find({ email: data.email });
+
+  if (verifyEmail.length > 0)
+    throw new AppError("O email já está cadastrado", 400);
+
   data.password = await hash(data.password, 10);
 
   const newUser = await UserModel.create(data);
